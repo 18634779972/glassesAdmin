5c558a325dbf4cbbd28e130f2d136db99d07af67
wenshuai <931145736@qq.com>
2019-10-25 11:54:31 +0800

    添加在线文档

--- src/main/java/com/xjt/dto/ActioncolumnReqDto.java
+++ src/main/java/com/xjt/dto/ActioncolumnReqDto.java
@@ -1,10 +1,15 @@
 package com.xjt.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel
 public class ActioncolumnReqDto extends BaseReqDto {
 
     /**
      * 主键id
      */
+    @ApiModelProperty(value = "主键id")
     private String actionColumnId;
 
     /**
